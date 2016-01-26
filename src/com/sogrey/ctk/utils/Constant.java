e217b7d9d08fbec3da5f4f45314848e2d444f112
sogrey <suzhaoheng2006@163.com>
2016-01-26 15:38:41 +0800

    app_key

---
+++ src/com/sogrey/ctk/utils/Constant.java
@@ -0,0 +1,13 @@
+/**
+ * @author Sogrey
+ * @date 2016-1-25下午5:52:01
+ */
+package com.sogrey.ctk.utils;
+
+/**
+ * @author Sogrey
+ * @date 2016-1-25下午5:52:01
+ */
+public class Constant {
+	public static final String api_key = "d78c4e496246c67a532ab130d15465ce";
+}
